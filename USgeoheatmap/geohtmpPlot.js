aad690dab4f21a46f158d3533465bd0f19a2b881
LeiHuang-statistician <105243946+LeiHuang-statistician@users.noreply.github.com>
2023-04-24 09:37:46 -0800

    Add files via upload

---
+++ USgeoheatmap/geohtmpPlot.js
@@ -0,0 +1,192 @@
+
+function geohtmp(){
+    var path = d3.geoPath();
+    var myColor = d3.scaleSequential()
+                    .interpolator(colortheme)
+                    .domain([rmin,rmax])
+
+    var svg = d3.select("#svg")
+      .attr("width", widthA + margin.left + margin.right)
+      .attr("height", heightA + margin.top + margin.bottom)
+      .append('g')
+      .attr('id', 'svg-forestplot')
+      // .attr('transform', `translate(${margin.left+80},${margin.top+30})`)
+    
+       stateG=d3.select("#svg-forestplot")
+              .append('g')
+             .attr("class", "states")
+    
+    //
+       stateG.selectAll(".pathid")
+        .data(states.features)
+        .enter()
+        .append('g')
+        .attr('class','pathid')
+        .attr('id', (d,i)=>`pathid${i}`)
+        .append("path")
+          .attr("d", path)
+          .attr("stroke","black")
+          .attr("fill", d=>ctl===1? "none":+d.properties.value===99999? "black" : myColor(+d.properties.value))
+    
+        svg.append("g")
+          .attr("class", "counties")
+          .selectAll("path")
+          .data(counties.features)
+          .enter().append("path")
+          .attr("d", path)
+          .attr("fill", d=>{ if (ctl===1) {
+                                if (+d.properties.value===99999) {return "black"}
+                                 else {return myColor(+d.properties.value)}
+                            } else {return "none"}
+                           })
+              .append('title')
+              .text(d=>`Map : ${d.properties.name} : ${d.id} , Your dataset: ${d.properties.FIPS}`);
+    
+        if (ctl===1){
+          svg.append("path")
+              .attr("class", "county-borders")
+              .attr("d", path(countiesborder))
+              .style("fill", "none")
+        }
+    
+          svg.append("path")
+              .attr("class", "state-borders")
+              .attr("d", path(statesborder))
+              .style("fill", "none")
+              .style("stroke","black")
+              .style("stroke-width",0.5);
+    
+          svg.selectAll("text")
+                .data(states.features)
+                .enter()
+                .append("text")
+                .attr("fill", "black")
+                .style('font-family','Helvetica')
+                .style('font-weight', 700)
+                .style("font-size",fontsize)
+                .attr("transform", function(d) {
+                    var stl;
+                     stl=stlabel(d.properties.name.toUpperCase())
+                    var centroid = path.centroid(d);
+                    if (stl==="FL") {return `translate(${centroid[0]+10},${centroid[1]})`}
+                    if (stl==="DC") {return `translate(${centroid[0]},${centroid[1]+5})`}
+                    if (stl==="MD") {return `translate(${centroid[0]},${centroid[1]-10})`}
+                    if (stl==="DE") {return `translate(${centroid[0]+5},${centroid[1]})`}
+                    if (stl==="RI") {return `translate(${centroid[0]+5},${centroid[1]+3})`}
+                    if (stl==="LA") {return `translate(${centroid[0]-5},${centroid[1]})`}
+                    if (stl==="NH") {return `translate(${centroid[0]+5},${centroid[1]})`}
+                    return `translate(${centroid[0]},${centroid[1]})`
+                })
+                .attr("text-anchor", "middle")
+                .attr("dy", ".35em")
+                .text(d=>{
+                     var stl;
+                     stl=stlabel(d.properties.name.toUpperCase())
+                     return stl})
+                .style("opacity", d=>vls===1? 1 : 0)
+    
+           var colors=[]
+           for (var i=rmin;i<=rmax;i++){
+               color=myColor(i)
+               colors.push(color)
+           }
+    //      colors = [ myColor(0),myColor(100)];
+    
+          var grad = svg.append('defs')
+            .append('linearGradient')
+            .attr('id', 'grad')
+            .attr('x1', '0%')
+            .attr('x2', '100%')
+            .attr('y1', '0%')
+            .attr('y2', '0%');
+    
+          grad.selectAll('stop')
+            .data(colors)
+            .enter()
+            .append('stop')
+            .style('stop-color', function(d){ return d; })
+            .attr('offset', function(d,i){
+              return 100 * (i / (colors.length - 1)) + '%';
+            })
+    
+          svg.append('rect')
+            .attr('width', 750)
+            .attr('height',20)  /*panel height*/
+            .style('fill', 'url(#grad')
+            .style("stroke", "black")
+            .style("stroke-width", 0.5)
+            .attr('transform', `translate(100,680)`)
+    
+          var xA = d3.scaleLinear()
+            .domain([rmin, rmax])
+            .range([0, 750])    /*right axis height*/
+            .nice()
+          var xAxis = d3.axisBottom(xA)
+            .ticks(6)
+            .tickSize(0)
+    
+          xAxidsG=svg.append('g')
+            .attr("id", "xAxidsG")
+            .attr("class", "xAxidsG")
+            .style("font-size",fontsize-2)
+            .attr('transform', `translate(100,703)`)
+            .call(xAxis)
+            .select(".domain").remove()
+    
+    // Add title to graph
+            var til=svg.append('g')
+            .attr('class','title')
+    
+            svg.selectAll(".tiltext").data(title)
+            .enter()
+             .append('text')
+             .attr("class", "tiltext")
+            .text(d=>d)
+            .style('font-size', fontsize)
+            .style('font-weight', 600)
+            .style('font-family','Helvetica')
+            .style("text-anchor", "left")
+            .attr('dy', -10)
+            .attr('transform',(d,i)=> `translate( ${titpos},680)`)
+    
+    
+      const svgS=document.getElementById("svg")
+      const {x,y,width,height}=svgS.viewBox.baseVal;
+      var svgData = $("#svg")[0].outerHTML;
+      var svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
+      var svgUrl = URL.createObjectURL(svgBlob);
+      const image=document.createElement('img');
+      image.src=svgUrl
+      //console.log(svgData)
+    
+      image.addEventListener('load', ()=>{
+          canvas=document.createElement('canvas')
+          canvas.width=width;
+          canvas.height=height;
+          context=canvas.getContext('2d')
+          context.drawImage(image,x,y,width, height)
+          //console.log('context',context)
+          const link=document.getElementById('link');
+          link.href=canvas.toDataURL();
+          //console.log(link)
+          URL.revokeObjectURL(svgUrl);
+      })
+    
+    
+      function printToCart2( ) {
+        let popupWinindow;
+        let innerContents = document.getElementById("svg").outerHTML;
+        popupWinindow = window.open();
+        popupWinindow.document.open();
+        popupWinindow.document.write('<body onload="window.print()">' + innerContents );
+        popupWinindow.document.close();
+      }
+      document.querySelector("#download").onclick = function(){
+      printToCart2()
+      }
+
+      $('#rmin').val(rmin);
+      $('#rmax').val(rmax);
+    }
+    
+    
---
+++ USgeoheatmap/geotest5.js
@@ -0,0 +1,180 @@
+var idarray = $("#sidebar") //get sidebar ids.
+             .find("img") //Find the spans
+             .map(function() { return this.id; }) //Project Ids
+             .get(); //ToArray
+console.log(idarray)
+
+function readfile(e){
+d3.json("https://unpkg.com/us-atlas@2.1.0/us/10m.json").then(function(us) {
+
+  //console.log(us.objects.states)
+
+file = document.getElementById("myfile").files[0];
+console.log(file)
+reader = new FileReader();
+reader.addEventListener('load', e => {
+    const csvData = e.target.result.toString();
+    //console.log('CSV Data:', csvData);
+    data=csvToJSON(csvData)
+    //console.log(data)
+    //console.log('data',data)
+    rHeader = csvData.split('\r').join('').split('\n')
+    Dheaders =rHeader[0].split(',');
+    coln=data.length
+    console.log('headers',Dheaders)
+    // console.log('data',data)
+    // x=data.filter(d=>+d.value===0)
+    // console.log('x',x)
+
+
+    const stateData=data.filter(d=>d.type==='state')
+    const countyData=data.filter(d=>d.type==='county')
+    //console.log(stateData)
+    //console.log(countyData)
+
+    title=[]
+    data.forEach(function(item){
+        d=item['title']
+        if (d!="")
+        {title.push(d)}
+    })
+    title.reverse();
+    //console.log(title)
+    countiesborder=topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })
+    statesborder=topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })
+    states=topojson.feature(us, us.objects.states)
+                  states.features.forEach((d,i)=>{
+                    var datafilter=stateData.filter(ditem=>+ditem.FIPS==+d.id)
+                    //console.log(datafilter)
+                    Object.assign(d.properties,datafilter[0]);
+                    });
+    counties=topojson.feature(us, us.objects.counties)
+                  counties.features.forEach((d,i)=>{
+                    var datafilter=countyData.filter(ditem=>+ditem.FIPS==+d.id)
+                    //console.log(datafilter)
+                    Object.assign(d.properties,datafilter[0]);
+                    });
+
+    eff=[]
+    data.forEach(function(item){
+    d=+item['value']
+    if (d<99999)
+    {eff.push(d)}
+    })
+
+    rmin=(+Math.min(...eff))
+    rmax=(+Math.max(...eff))
+    console.log('rmin',rmin)
+    console.log('rmin',rmax)
+   
+  
+   
+  
+
+    margin = {top: -30, right: 20, bottom: 50, left: 150};
+    widthA = 1000;
+    heightA = 700;
+    fontsize=18;
+    colortheme=d3.interpolateReds;
+
+    vls=1;
+    ctl=1;
+    function alerfs() {
+      alert("Do not find county data. Will show state map instead!");
+    }
+    function alerfc() {
+      alert("Do not find state data. Will show county map instead!");
+    }
+    if (countyData.length===0) {
+      ctl=0;
+      alerfs()
+    }
+
+
+    titpos=100;
+
+    geohtmp()
+
+    $('.vls').change(function() {
+      //Use $option (with the "$") to see that the variable is a jQuery object
+      var $option = $(this).find(':selected');
+      //Added with the EDIT
+      var value = $option.val();//to get content of "value" attrib
+      if (value==="Yes"){vls=1}
+      else if (value==="No"){vls=0 }
+      $("#svg-forestplot").remove();
+      geohtmp()
+    })
+
+    divlist=[".titpos"]
+
+    for (let i = 0; i < divlist.length; i++) {
+      $(divlist[i]).mouseup(function(){
+      //    if (i==0){discval=this.value}
+      //    if (i==1){rrval=this.value}
+      //    if (i==2){plotval=this.value}
+      if (i==0){titpos=this.value}
+      $("#svg-forestplot").remove();
+      geohtmp()
+      });
+    }
+
+    $(".titpos").keydown(function(e){
+      switch (e.which){
+       case 37: titpos=this.value;break;
+       case 39: titpos=this.value;break;
+      }
+      $("#svg-forestplot").remove();
+      geohtmp()
+     });
+
+    $('.level').change(function() {
+      //Use $option (with the "$") to see that the variable is a jQuery object
+      var $option = $(this).find(':selected');
+      //Added with the EDIT
+      var value = $option.val();//to get content of "value" attrib
+      if (value==="Yes"){ctl=1}
+      else if (value==="No"){ctl=0 }
+      $("#svg-forestplot").remove();
+      if (stateData.length===0) {
+      ctl=1;
+      alerfc()
+          }
+      geohtmp()
+    })
+
+    
+    $(".rmin").keyup(function(){
+      rmin=this.value;
+      $("#svg-forestplot").remove();
+    geohtmp()
+    });
+
+    $(".rmax").keyup(function(){
+      rmax=this.value;
+      $("#svg-forestplot").remove();
+    geohtmp()
+    });
+
+    for (let i=0; i<idarray.length;i++){
+      $('#'+idarray[i]).click(function(){
+          if (i===0) {colortheme=d3.interpolateBlues}
+          if (i===1) {colortheme=d3.interpolateGreens}
+          if (i===2) {colortheme=d3.interpolateOranges}
+          if (i===3) {colortheme=d3.interpolatePurples}
+          if (i===4) {colortheme=d3.interpolateReds}
+          if (i===5) {colortheme=d3.interpolateBrBG}
+          if (i===6) {colortheme=d3.interpolatePiYG}
+          if (i===7) {colortheme=d3.interpolateRdBu}
+          if (i===8) {colortheme=d3.interpolateRdYlGn}
+          
+          $("#svg-forestplot").remove();
+          geohtmp()
+      })
+    }
+
+  })
+ reader.readAsText(file, 'UTF-8')
+})
+}
+
